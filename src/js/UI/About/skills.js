d873d7d5077b233335f79d784152703f23b03915
and1356jin <and1356.jin@gmail.com>
2024-06-10 03:26:37 -0500

    [dex]

---
+++ src/js/UI/About/skills.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        name: 'React/NextJS',
+        width: '100%',
+    },
+    {
+        name: 'Vue/NuxtJs',
+        width: '100%',
+    },
+    {
+        name: 'Angular',
+        width: '95%'
+    },
+    {
+        name: 'Laravel',
+        width: '90%',
+    },
+    {
+        name: 'Django',
+        width: '90%',
+    },
+    {
+        name: 'Wordpress',
+        width: '90%',
+    },
+    {
+        name: 'Swift',
+        width: '95%',
+    },
+    {
+        name: 'Firebase',
+        width: '85%',
+    },
+]
---
+++ src/js/UI/Work/items.js
@@ -0,0 +1,72 @@
+export default [
+  {
+    id: 0,
+    name: "Operate at a higher level",
+    description: "The leading virtual reality surgical training and assessment platform.",
+    image: "images/projects/osso-heco.jpg",
+    tags: ["VR", "React", "Nodejs", "TailwindCSS"],
+    liveview: "https://osso-heco.webflow.io/",
+    alt: "Hospital website portfolio project",
+  },
+  {
+    id: 1,
+    name: "CryptoMines",
+    description: "Mint NFTs, Explore Planets",
+    image: "images/projects/cryptomines.jpg",
+    tags: ["Web3", "React", "Nodejs", "TailwindCSS","Solidity"],
+    liveview: "https://cryptomines.app/",
+    alt: "Mint NFTs, Explore Planets portfolio project",
+  },
+  {
+    id: 2,
+    name: "Food Delivery",
+    description: "Food Delivery Groceries, shops, pharmacies, anything!",
+    image: "images/projects/glovoapp.jpg",
+    tags: ["Vue", "Nuxt", "Laravel", "TailwindCSS"],
+    liveview: "https://glovoapp.com/",
+    alt: "Food Delivery portfolio project",
+  },
+  {
+    id: 3,
+    name: "Islee",
+    description: `Best Dress. Whenever, Wherever.`,
+    image: "images/projects/Islee.png",
+    tags: ["React", "Wordpress", "Mysql"],
+    liveview: "https://islee.com.my/",
+    alt: "Islee portfolio porject",
+    bannerIcons: [
+      { src: "images/cssda-icon.png", alt: "Best Dress. Whenever, Wherever." },
+    ],
+  },
+  {
+    id: 4,
+    name: "Drumelia LUXURY REAL ESTATE",
+    description:
+      "This is the Luxury Real Estate website",
+    liveview: "https://www.drumelia.com/",
+    image: "images/projects/drumelia.jpg",
+    tags: ["Django", "Mysql", "Vue", "TailwindCSS"],
+    alt: "This is the Luxury Real Estate website",
+  },
+  {
+    id: 5,
+    name: "UR Clinic",
+    description:
+      "A centralized platform for appointment management system, treatment information, ...",
+    liveview: "https://member.urskin.com.my/",
+    image: "images/projects/UR Klinik.jpg",
+    tags: ["Angular", "TypeScript", "html", "css"],
+    alt: "UR Clinic",
+  },
+  {
+    id: 6,
+    name: "NFT Show Room",
+    description:
+      "NFT Show Room",
+    image: "images/projects/nftshowroom.jpg",
+    liveview: "https://nftshowroom.com/",
+    tags: ["Vue", "Nuxt", "Nodejs", "TailwindCSS"],
+    alt: "NFT Show Room",
+  },
+  
+];
